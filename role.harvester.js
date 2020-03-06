b6d0aec98cada1421cfc2f0055207b1e5e321cba
bestkurisu <37394086+bestkurisu@users.noreply.github.com>
2020-03-06 13:44:24 +0800

    Update role.harvester.js

--- role.harvester.js
+++ role.harvester.js
@@ -1,11 +1,11 @@
-var roleHarvesteer = {
+var roleHarvester = {
 
     /** @param {Creep} creep **/
     run: function(creep) {
         if(creep.store.getFreeCapacity() > 0) {
             var sources = creep.room.find(FIND_SOURCES);
-            if(creep.harvest(sources[1]) == ERR_NOT_IN_RANGE) {
-                creep.moveTo(sources[1]);
+            if(creep.harvest(sources[0]) == ERR_NOT_IN_RANGE) {
+                creep.moveTo(sources[0], {visualizePathStyle: {stroke: '#ffaa00'}});
             }
         }
         else {
@@ -14,17 +14,17 @@ var roleHarvesteer = {
                     return (structure.structureType == STRUCTURE_EXTENSION ||
                         structure.structureType == STRUCTURE_SPAWN ||
                         structure.structureType == STRUCTURE_TOWER ||
-                        structure.structureType == STRUCTURE_CONTAINER) &&
+                        structure.id == '5e5fc193a8bd372dc8b835b5') &&
                         structure.store.getFreeCapacity(RESOURCE_ENERGY) > 0;
                 }
             });
             }
             if(targets) {
                 if(creep.transfer(targets, RESOURCE_ENERGY) == ERR_NOT_IN_RANGE) {
-                    creep.moveTo(targets);
+                    creep.moveTo(targets, {visualizePathStyle: {stroke: '#ffffff'}});
                 }
             }
     }
 };
 
-module.exports = roleHarvesteer;
+module.exports = roleHarvester;
