3329f96b87f99465e7805eb6dcd11ee82cfcbfda
bestkurisu <37394086+bestkurisu@users.noreply.github.com>
2020-03-06 13:45:34 +0800

    Update role.updater.js

--- role.updater.js
+++ role.updater.js
@@ -18,12 +18,26 @@ var roleUpgrader = {
             }
         }
         else {
-            var sources = creep.room.find(FIND_SOURCES);
-            if(creep.harvest(sources[1]) == ERR_NOT_IN_RANGE) {
-                creep.moveTo(sources[1], {visualizePathStyle: {stroke: '#ffaa00'}});
+            let container = creep.pos.findClosestByPath(FIND_STRUCTURES, {
+                filter: s => (s.id == '5e606de0e4c9ff6ef3b1dca5' || s.id == '5e5ff70811a570efde5aa774') &&
+                             s.store[RESOURCE_ENERGY] > 0
+            });
+            if (container != undefined) {
+                // try to withdraw energy, if the container is not in range
+                if (creep.withdraw(container, RESOURCE_ENERGY) == ERR_NOT_IN_RANGE) {
+                    // move towards it
+                    creep.moveTo(container);
+                }
+            }
+            else{
+                creep.say('warning');
+                //var sources = creep.room.find(FIND_SOURCES);
+                //if(creep.harvest(sources[1]) == ERR_NOT_IN_RANGE) {
+                //    creep.moveTo(sources[1], {visualizePathStyle: {stroke: '#ffaa00'}});
+                //}
             }
         }
     }
-};
+}
 
 module.exports = roleUpgrader;
