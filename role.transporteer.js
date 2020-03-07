c59928aed41038d5b4400d038617ad52c36d2833
bestkurisu <37394086+bestkurisu@users.noreply.github.com>
2020-03-07 11:52:18 +0800

    Update role.transporteer.js

--- role.transporteer.js
+++ role.transporteer.js
@@ -1,4 +1,4 @@
-var roleTransporteer = {
+var roleTransporter = {
 
     /** @param {Creep} creep **/
     run: function(creep) {
@@ -10,25 +10,24 @@ var roleTransporteer = {
                 }
             }
             else {
-                var targets = creep.room.find(FIND_STRUCTURES, {
-                    filter: (i) => i.id == '5e5f6c33ea09a695a34fb7ed'
+                var targets = creep.pos.findClosestByPath(FIND_STRUCTURES, {
+                    filter: (i) => (i.id == '5e5fc193a8bd372dc8b835b5' || i.id == '5e5f6c33ea09a695a34fb7ed' ) && i.store[RESOURCE_ENERGY] > 450
                 });
-                if(targets.length > 0) {
-                    if(creep.withdraw(targets[0], RESOURCE_ENERGY) == ERR_NOT_IN_RANGE) {
-                        creep.moveTo(targets[0]);
+                if(targets) {
+                    if(creep.withdraw(targets, RESOURCE_ENERGY) == ERR_NOT_IN_RANGE) {
+                        creep.moveTo(targets, {visualizePathStyle: {stroke: '#ffaa00'}});
                     }
                 }
             }
         }
         else {
             var targets = creep.pos.findClosestByPath(FIND_STRUCTURES, {
-                filter: (i) => (i.structureType == STRUCTURE_EXTENSION && i.store[RESOURCE_ENERGY] < 50) || (i.structureType == STRUCTURE_SPAWN && i.store[RESOURCE_ENERGY] < 300) || (i.structureType == STRUCTURE_TOWER && i.store[RESOURCE_ENERGY] < 600)
+                filter: (i) => (i.structureType == STRUCTURE_EXTENSION && i.store[RESOURCE_ENERGY] < 50) || (i.structureType == STRUCTURE_SPAWN && i.store[RESOURCE_ENERGY] < 300) ||(i.structureType == STRUCTURE_TOWER && i.store[RESOURCE_ENERGY] < 600 ) 
             });
             if (targets == null) {
-                targets = creep.room.find(FIND_STRUCTURES, {
-                    filter: (i) => (i.id == '5e606de0e4c9ff6ef3b1dca5' || i.id == '5e5ff70811a570efde5aa774') && i.store[RESOURCE_ENERGY] < 2000
+                targets = creep.pos.findClosestByPath(FIND_STRUCTURES, {
+                    filter: (i) => (i.id == '5e606de0e4c9ff6ef3b1dca5' || i.id == '5e5ff70811a570efde5aa774') && i.store[RESOURCE_ENERGY] < 1600
                 });
-                targets = targets[0]
             }
             if (targets == null) {
                 targets = creep.pos.findClosestByPath(FIND_STRUCTURES, {
@@ -44,4 +43,4 @@ var roleTransporteer = {
 	} 
 };
 
-module.exports = roleTransporteer;
+module.exports = roleTransporter;
