a95b7eb842903405e929d7a64ba311821c979211
Bestkurisu <37394086+bestkurisu@users.noreply.github.com>
2020-05-20 18:46:23 +0800

    push code

--- src/room/lab.js
+++ src/room/lab.js
@@ -5,7 +5,7 @@ var extension={
     * @returns {Number} -1:目标错误, -2:lab不足,
     * 建议定时清除重新检查
     */
-    getFeederLabs=function(){
+    getFeederLabs:function(){
         if(!!this.memory.feederLabs){
             var feederlabs=getGameObject(this.memory.feederLabs)
             if(feederlabs.length === 2){
@@ -53,7 +53,7 @@ var extension={
     * @returns {Number} -1:目标错误, -2:lab不足,
     * 建议定时清除重新检查
     */
-    getVatLabs=function(){
+    getVatLabs:function(){
         var feederLabs=this.getFeederLabs()
         if(!feederlabs){
             return -1
@@ -63,7 +63,7 @@ var extension={
             if(labs.length>0){
                 for(let i=0;i<labs.length;i++){
                     if(!labs[i].memory || labs[i].memory){
-                        labs[i].memory={state:0}
+                        labs[i].memory={state:2}
                     }
                 }
                 return getGameObject(vatLabs)
@@ -74,7 +74,7 @@ var extension={
         }
         const vatlabs=this.find(FIND_STRUCTURES,{
             filter: s => s.strutureType === STRUCTURE_LAB &&
-                    (!s.memory || s.memory.state === 0)
+                    (!s.memory || s.memory.state === 2)
         })
         this.memory.vatLabs=setGameID(vatlabs)
         return getGameObject(vatlabs)
@@ -82,20 +82,83 @@ var extension={
 
     /**
     * 获取当前反应
-    * @returns {Object}
-    * @returns {Number} -1:目标错误, -2:lab不足,
-    * 建议定时清除重新检查
+    * @returns {Array}
+    * @returns {Number} 0:任务已完成, -1:无任务
     */
-    getActiveReaction=function(){
+    getActiveReaction:function(){
         const reaction=this.memory.reaction
         if(!reaction){
             return -1
         }
-        var target=reaction.compound
+        const target=reaction.compound
+        const amount=reaction.amount
+        const count=reaction.count
+        if(!reaction.ready){
+            this.memory.reaction.labState=0
+            this.memory.reaction.ready=true
+        }
         mid=REACTION[target]
-        
+        var queue=[]
+        var length=1
+        queue.push([target,mid])
+        for(let i=0;i<mid.length;i++){
+            if(mid[i] in REACTION && mid[i] !== 'G'){
+                length=2
+                var target1=mid[i]
+                var mid1=REACTION[target1]
+                queue.push([target1,mid1])
+                for(let j=0;j<mid1.length;j++){
+                    if(mid1[i] in REACTION && mid1[i] !== 'G'){
+                        length=3
+                        var target2=mid1[i]
+                        var mid2=REACTION[target2]
+                        queue.push([target2,mid2])
+                    }
+                }
+            }
+        }
+        queue.reverse()
+        if(count>=amount){
+            this.memory.reaction.count=0
+            if(this.memory.reaction.labState<queue.length){
+                this.memory.reaction.labState++
+            }
+            else{
+                this.memory.reaction=undefined
+                return 0
+            }
+        }
+        return queue[this.memory.labState]
+    },
+
+    /**
+    * 设置boostlab，注意lab状态需要creep boost完成后修改，建议定期检查
+    * @returns {Number} 0:任务已完成
+    */
+    setBoostLab:function(){
+        var boostTask=this.memory.boostTask
+        if(!boostTask || boostTask.length === 0){
+            return
+        }
+        var i=0
+        var labs=this.getVatLabs()
+        if(!labs || labs.length === 0){
+            return
+        }
+        while(i<labs.length){
+            if(boostTask.length>0){
+                labs[i].memory={
+                    state:3,
+                    compound:boostTask.shift()
+                }
+            }
+            else{
+                break
+            }
+        }
+        return 0
     }
 }
-module.exports = function () {
-    _.assign(Room.prototype, extension)
-}
+
+
+_.assign(Room.prototype, extension)
