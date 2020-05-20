26a6b288e6c07d01850171e9e06e58fbf461bdfc
Bestkurisu <37394086+bestkurisu@users.noreply.github.com>
2020-05-20 18:47:08 +0800

    Update conflict.js

---
+++ src/room/conflict.js
@@ -0,0 +1,20 @@
+var whitelist=require('whitelist')
+var extension={
+    /**
+    * 搜索房间敌人
+    * @returns {Array}
+    * @returns {Number} 0:任务已完成, -1:无任务
+    */
+    getHostileCreep=function(){
+        var creep=this.find(FIND_HOSTILE_CREEPS,{
+            filter: c => !(c.owner.username in whitelist) &&
+                        c.owner.username !== 'Invader' 
+        })
+        var npc=this.find(FIND_HOSTILE_CREEPS,{
+            filter: c => c.owner.username === 'Invader'
+        })
+    }
+}
+
+
+_.assign(Room.prototype, extension)
