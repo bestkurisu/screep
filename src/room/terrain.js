ef23ca6568424d65abb7ea4373e7b97ffc32ec94
bestkurisu <37394086+bestkurisu@users.noreply.github.com>
2020-06-09 00:31:15 +0800

    code

---
+++ src/room/terrain.js
@@ -0,0 +1,6 @@
+Room.Terrain.prototype.isWalkable = function (x, y) {
+    return !(this.get(x, y) & TERRAIN_MASK_WALL)
+}
+Room.Terrain.prototype.isSwamp = function (x, y) {
+    return (this.get(x, y) === TERRAIN_MASK_SWAMP)
+}
