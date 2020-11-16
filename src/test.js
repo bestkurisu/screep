af0a7e7dadf1a9fc9c792bbb313504699b28aacc
bestkurisu <37394086+bestkurisu@users.noreply.github.com>
2020-11-16 21:37:00 +0800

    nothing

--- src/test.js
+++ src/test.js
@@ -1,26 +1,18 @@
-module.exports=function(){
-    if(Game.time%10==0){
-        var order = Game.market.getAllOrders(order =>
-            order.resourceType == RESOURCE_ENERGY && order.type == ORDER_BUY)
-        order.sort((a,b)=>b.price-a.price)
-        var price = Math.min(0.07,order[0].price)
-        var deal=false
-        if(Game.rooms['W59N31].storage.store['energy']<400000){
-            var order = Game.market.getAllOrders(order => order.resourceType == RESOURCE_ENERGY &&
-                order.type == ORDER_SELL && order.amount>30000)
-            for(let j=0;j<order.length;j++){
-                order[j].realPrice=order[j].amount*order[j].price/(order[j].amount-Game.market.calcTransactionCost(order[j].amount, 'W29N5', order[j].roomName))
-            }
-            if(order.length>0){
-                order.sort((a,b)=>a.realPrice-b.realPrice)
-                if(order[0].realPrice<price){
-                    if(Game.market.deal(order[0].id,30000,'W59N31')==0){
-                        console.log('W59N31'+'买了'+(30000-Game.market.calcTransactionCost(30000,'W59N31', order[0].roomName))+ '能量')
-                    }
-                    else{
-                        deal=true
+var roomlist=[]
+if(Game.time % 10 === 0){
+    var room=Game.rooms[name]
+    if(room.controller.level == 8){
+        if(room.terminal && room.terminal.store['energy']>25000){
+            var orders = Game.market.getAllOrders({type: ORDER_BUY, resourceType: RESOURCE_ENERGY})
+            if(orders.length>0){
+                for(let i=0;i<orders.length;i++){
+                    orders[i].realPrice=orders[i].amount*orders[i].price/(orders[i].amount+Game.market.calcTransactionCost(orders[i].amount, room.name, orders[i].roomName))
+                    if(roomlist.indexOf(orders[i].roomName) !== -1){
+                        orders[i].realPrice = orders[i].realPrice * 2
                     }
                 }
+                orders.sort((a,b)=>b.realPrice-a.realPrice)
+                Game.market.deal(orders[0].id,Math.min(15000,orders[0].amount),room.name)
             }
         }
     }
